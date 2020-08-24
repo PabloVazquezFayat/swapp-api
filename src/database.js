8886fc77eb8ca2bb13edc42a7ffc32a786b797c6
Pablo Vazquez <pvazquez@vandldigital.com>
2020-08-24 16:42:12 -0400

    upated config in database connection method

--- src/database.js
+++ src/database.js
@@ -5,7 +5,7 @@ module.exports = ()=> {
     //connect to mongodb
     mongoose.set('useCreateIndex', true);
 
-    mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true , useFindAndModify: false })
     .then(x => {
         console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
     })
