fcee02c857278aebd7d9a41439f8953556b5c3b2
Pablo Vazquez <pvazquez@vandldigital.com>
2020-09-10 00:34:32 -0400

    search controller wip

--- controllers/search/search.js
+++ controllers/search/search.js
@@ -5,31 +5,42 @@ const Starship = require('../../models/Starship');
 const Vehicle = require('../../models/Vehicle');
 
 module.exports = async (req, res, next)=> {
+
+    function combineArrays(arrays){
+        const arr = [];
+        arrays.forEach(ar => {
+            ar.forEach(element => arr.push(element));
+        });
+        return arr;
+    }
+
     try{
 
-        const resultsPerPage = 10;
-        const currentPage = parseInt(req.params.page) - 1;  
-        const count = await Character.count();
+        const characterCount = await Character.countDocuments();
+        const planetCount = await Planet.countDocuments();
+        const speciesCount = await Species.countDocuments();
+        const starshipCount = await Starship.countDocuments();
+        const vehicleCount = await Vehicle.countDocuments();
 
-        const characters = await Character.find()
-            .skip(resultsPerPage * currentPage)
-            .limit(resultsPerPage)
-            .populate({path: 'homeWorld'})
-            .populate({path: 'vehicles'})
-            .populate({path: 'starship'})
+        const count = characterCount + planetCount + speciesCount + starshipCount + vehicleCount;
 
-        const planets = await Planets.find()
-            .skip(resultsPerPage * currentPage)
-            .limit(resultsPerPage);
+        const characters = await Character.find({$or:[{name: {$regex : `.*${req.body.search}.*`, '$options' : 'i'}}]});
+        const planets = await Planet.find({$or:[{name: {$regex : `.*${req.body.search}.*`, '$options' : 'i'}}]})
+        const species = await Species.find({$or:[{name: {$regex : `.*${req.body.search}.*`, '$options' : 'i'}}]})
+        const starships = await Starship.find({$or:[{name: {$regex : `.*${req.body.search}.*`, '$options' : 'i'}}]});
+        const vehicles = await Vehicle.find({$or:[{name: {$regex : `.*${req.body.search}.*`, '$options' : 'i'}}]});
 
-        const 
+        const allData = combineArrays([characters, planets, species, starships, vehicles]);
 
         const data = {
             count: count,
             data: allData
         }
-        
-        //return formatted data
+
+        if(allData){
+            res.status(200).json(data);
+            return;
+        }
 
     }catch(error){
         next(error)
